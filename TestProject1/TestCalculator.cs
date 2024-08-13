e5e1e2604b4b026bb20664be45d6304c5060a877
viktoriyadi <viksitoyyy@abv.bg>
2024-08-13 23:55:57 +0300

    Innitial commit

---
+++ TestProject1/TestCalculator.cs
@@ -0,0 +1,94 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+using System;
+
+namespace TestProject1
+{
+    [TestFixture]
+    public class TestCalculator
+    {
+        IWebDriver driver;
+        IWebElement textBoxFirstNum;
+        IWebElement textBoxSecondNum;
+        IWebElement dropDownOperation;
+        IWebElement calcBtn;
+        IWebElement resetBtn;
+        IWebElement divResult;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("headless");
+            chromeOptions.AddArguments("no-sandbox");
+            chromeOptions.AddArguments("disable-dev-shm-usage");
+            chromeOptions.AddArguments("disable-gpu");
+            chromeOptions.AddArguments("window-size=1920x1080");
+            chromeOptions.AddArguments("disable-extension");
+            chromeOptions.AddArguments("remote-debugging-port=9222");
+
+            chromeOptions.AddUserProfilePreference("profile.password_manager_enabled", false);
+            chromeOptions.AddArgument("--disable-search-engine-choice-screen");
+
+            driver = new ChromeDriver(chromeOptions);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Url = "http://softuni-qa-loadbalancer-2137572849.eu-north-1.elb.amazonaws.com/number-calculator/";
+
+            textBoxFirstNum = driver.FindElement(By.Id("number1"));
+            dropDownOperation = driver.FindElement(By.Id("operation"));
+            textBoxSecondNum = driver.FindElement(By.Id("number2"));
+            calcBtn = driver.FindElement(By.Id("calcButton"));
+            resetBtn = driver.FindElement(By.Id("resetButton"));
+            divResult = driver.FindElement(By.Id("result"));
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        public void PerformCalculation(string firstNumber, string operation,
+                                        string secondNumber, string expectedResult)
+        {
+            // Click the [Reset] button
+            resetBtn.Click();
+
+            // Send values to the corresponding fields if they are not empty
+            if (!string.IsNullOrEmpty(firstNumber))
+            {
+                textBoxFirstNum.SendKeys(firstNumber);
+            }
+
+            if (!string.IsNullOrEmpty(secondNumber))
+            {
+                textBoxSecondNum.SendKeys(secondNumber);
+            }
+
+            if (!string.IsNullOrEmpty(operation))
+            {
+                new SelectElement(dropDownOperation).SelectByText(operation);
+            }
+
+            // Click the [Calculate] button
+            calcBtn.Click();
+
+            // Assert the expected and actual result text are equal
+            Assert.That(divResult.Text, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        [TestCase("5", "+ (sum)", "10", "Result: 15")]
+        [TestCase("3.5", "- (subtract)", "1.2", "Result: 2.3")]
+        [TestCase("2e2", "* (multiply)", "1.5", "Result: 300")]
+        [TestCase("5", "/ (divide)", "0", "Result: Infinity")]
+        [TestCase("invalid", "+ (sum)", "10", "Result: invalid input")]
+        public void TestNumberCalculator(string firstNumber, string operation,
+                                            string secondNumber, string expectedResult)
+        {
+            PerformCalculation(firstNumber, operation, secondNumber, expectedResult);
+        }
+    }
+}
